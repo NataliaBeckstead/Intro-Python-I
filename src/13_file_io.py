bedd5211cf16494adfa11977d108eaaaa038d50d
Natalia Beckstead <toosmiravilla@gmail.com>
2020-07-07 13:36:12 -0600

    scopes and file finished

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+"""
+It is good practice to use the with keyword when dealing with file objects. 
+The advantage is that the file is properly closed after its suite finishes, even if an exception is raised at some point.
+"""
+with open('src/foo.txt') as foo:
+    print(foo.read())
+print(foo.closed) # Checking if foo closed with "with"
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open("src/bar.txt", "w") as bar:
+    bar.write("1st line\n2nd line\n3rd line\n")
